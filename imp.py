dea106e087bf26633a798cb8e592e41950f25ddb
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-04-26 07:31:41 +0200

    Change CLI

--- imp.py
+++ imp.py
@@ -11,12 +11,12 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('--fliplr', help='flip horizontally', action='store_true')
 group.add_argument('--flipud', help='flip vertically', action='store_true')
 group.add_argument('--inverse', help='invert colors', action='store_true')
-group.add_argument('--rgb2gray', help='convert RGB to grayscale', action='store_true')
+group.add_argument('--grayscale', help='convert to grayscale', action='store_true')
 group.add_argument('--bright', help='change brightness BRIGHTNESS is float in [-1, 1]', type=float)
 group.add_argument('--sharpen', help='sharpen', action='store_true')
-group.add_argument('--edge_detection', help='detect edges', action='store_true')
-group.add_argument('--box_blur', help='apply box blur', action='store_true')
-group.add_argument('--gaussian_blur', help='apply gaussian blur', action='store_true')
+group.add_argument('--edge', help='apply kernel which detects edges', action='store_true')
+group.add_argument('--box', help='apply box blur', action='store_true')
+group.add_argument('--gaussian', help='apply gaussian blur', action='store_true')
 parser.add_argument('INPUT', help='input image')
 parser.add_argument('OUTPUT', help='output image')
 args = parser.parse_args()
@@ -33,17 +33,17 @@ elif args.flipud:
     img = imp.operation.flipud(img)
 elif args.inverse:
     img = imp.operation.inverse(img)
-elif args.rgb2gray:
+elif args.grayscale:
     img = imp.operation.rgb2gray(img)
 elif args.bright:
-    img = imp.operation.brightness(img, args.brightness)
+    img = imp.operation.brightness(img, args.bright)
 elif args.sharpen:
     img = imp.operation.sharpen(img)
-elif args.edge_detection:
+elif args.edge:
     img = imp.operation.edge_detection(img)
-elif args.box_blur:
+elif args.box:
     img = imp.operation.box_blur(img)
-elif args.gaussian_blur:
+elif args.gaussian:
     img = imp.operation.gaussian_blur(img)
 
 # read output image
